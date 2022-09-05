3b8ec9d25f31b9464e48002e9e5439b574d3b940
costrategix-anirudh-das <anirudh.das@costrategix.com>
2022-09-06 03:04:53 +0530

    Challenges

---
+++ src/factories_own_example/EmailNotification.java
@@ -0,0 +1,11 @@
+package factories_own_example;
+
+
+public class EmailNotification implements Notification {
+ 
+    @Override
+    public void notifyUser()
+    {
+        System.out.println("Sending an e-mail notification");
+    }
+}
---
+++ src/singleton_own_example/SingletonPatternDemo.java
@@ -0,0 +1,18 @@
+
+// Get the only object from the singleton class.
+package singleton_own_example;
+
+public class SingletonPatternDemo {
+	   public static void main(String[] args) {
+
+	      //illegal construct
+	      //Compile Time Error: The constructor SingleObject() is not visible
+	      //SingleObject object = new SingleObject();
+
+	      //Get the only object available
+	      SingleObject object = SingleObject.getInstance();
+
+	      //show the message
+	      object.showMessage();
+	   }
+	}
